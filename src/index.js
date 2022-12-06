cc6c8fede5541b3ba5ed0d2bd49626b8dd91ca52
¨kericho¨ <lvisrono90@gmail.com>
2022-12-06 06:15:16 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("craete-task-form").addEventListener("submit", (e) =>{
+    e.preventDefault()
+    submitBtn(e.target.input.value)
+  })
 });
+
+function submitBtn(task){
+  let ul = document.getElementById('tasks')
+  let li = document.createElement("li")
+  ul.appendChild(li)
+  li.inerText =`${task}`
+  let x = document.createElement('button')
+  x.addEventListener("click", deleteElem)
+  x.innerText= 'X'
+  li.appendChild(x)
+}
+
+function deleteElem(e){
+  e.target.parentNode.remove()
+}
