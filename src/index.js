671278b2f3732b583d35e7a1690c5406a60a58ec
¨kericho¨ <lvisrono90@gmail.com>
2022-12-08 17:55:40 +0300

    readed some functionalities

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.getElementById("craete-task-form").addEventListener("submit", (e) =>{
+  document.getElementById("create-task-form").addEventListener("submit", (e)=>{
     e.preventDefault()
     submitBtn(e.target.input.value)
   })
-});
+ 
+})
+  function submitBtn(task){
+    let ul = document.getElementById('tasks')
+    let li = document.createElement('li')
+    ul.appendChild(li)
+    li.innerText = `${task} `
+    let x = document.createElement('button')
+    x.addEventListener("click", deleteElem)
+    x.innerText= 'X'
+    li.appendChild(x)
+  }
 
-function submitBtn(task){
-  let ul = document.getElementById('tasks')
-  let li = document.createElement("li")
-  ul.appendChild(li)
-  li.inerText =`${task}`
-  let x = document.createElement('button')
-  x.addEventListener("click", deleteElem)
-  x.innerText= 'X'
-  li.appendChild(x)
-}
+  function deleteElem(e){
+    e.target.parentNode.remove()
+  }
 
-function deleteElem(e){
-  e.target.parentNode.remove()
-}
